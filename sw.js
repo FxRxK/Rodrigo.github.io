4bcdb2ee7f5b1c033c76d2da0b6c4d51a6a9425a
FxRxK <58606433+FxRxK@users.noreply.github.com>
2023-11-24 07:38:45 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -37,6 +37,18 @@ self.addEventListener('install',e =>{
             .catch(err => console.log('no se ha registrado cache.',err))
         );
 });
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open('my-cache').then((cache) => {
+        return cache.addAll([
+          '/index.html',
+          '/styles.css',
+          '/script.js',
+          // Otros recursos
+        ]);
+      })
+    );
+  });
 //evento activate
 //Que la app funcione sin conexion
 self.addEventListener('activate',e=>{
